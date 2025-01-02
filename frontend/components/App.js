1f67722ba75fbcd00730c1007d5bf50a8d27dd21
Michael Gibson <selfmadecultureinc@gmail.com>
2025-01-02 02:46:41 -0600

    Nasa_API

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiKey = 'DEMO_KEY';
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 function App() {
+  const [apod, setApod] = useState(null);
+
+  useEffect(() => {
+    function fetchPhoto() {
+      axios
+        .get(URL)
+        .then((res) => {
+          setApod(res.data);
+        })
+        .catch((err) => {
+          console.error('Error fetching data:', err.message);
+        });
+    }
+
+    fetchPhoto();
+  }, []);
+
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
-  )
+    <div>
+      <h1>NASA Astronomy Picture of the Day</h1>
+      {apod ? (
+        <div>
+          <h2>{apod.title}</h2>
+          <img src={apod.url} alt={apod.title} />
+          <p>{apod.explanation}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
